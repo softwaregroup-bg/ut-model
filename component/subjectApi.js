4ba4e2081a6a14af491117177649aaa609373c6c
Kalin Krustev <kalin.krustev@gmail.com>
2023-11-30 16:12:20 +0200

    fix: use openapi

--- component/subjectApi.js
+++ component/subjectApi.js
@@ -15,20 +15,20 @@ module.exports = ({
             const api = await subjectApiGet({
                 $http: {
                     httpMethod: 'GET',
-                    uri: `/aa/api/${subject}/swagger.json`
+                    uri: `/aa/api/${subject}/openapi.json`
                 }
             }, utMeta());
             Object.values(api.paths).forEach(path =>
                 Object.values(path).forEach(method => {
                     if (method.operationId) {
                         params[method.operationId] = {};
-                        const schema = method?.parameters?.find(({in: where}) => where === 'body');
+                        const schema = method?.requestBody?.content?.['application/json'];
                         if (schema?.schema?.properties?.jsonrpc) {
                             if (schema?.schema?.properties?.params) params[method.operationId].params = schema.schema.properties.params;
                         } else {
                             if (schema?.schema) params[method.operationId].params = schema.schema;
                         }
-                        params[method.operationId].result = method?.responses?.['200']?.schema?.properties?.result;
+                        params[method.operationId].result = method?.responses?.['200']?.content?.['application/json'].schema?.properties?.result;
                     }
                 })
             );
